7296ae78fa935fc623fb55655d13bf7f5ff7b61e
dominicflash <prudvee.kotnana@valuelabs.net>
2015-06-11 12:49:47 +0530

    First commit

---
+++ LabelPadding/LabelPadding/FirstPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LabelPadding
+{
+	public partial class FirstPage : ContentPage
+	{
+		public FirstPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
